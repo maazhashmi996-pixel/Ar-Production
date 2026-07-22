678fc9ce853702f1cc27ac1cabe5388815d4e613
Muhammad Umair <152801250+UmairDogar10@users.noreply.github.com>
2026-07-22 16:58:29 +0500

    footer logo

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="border-t border-slate-200/80 bg-white/80 backdrop-blur-sm">
       <div className="max-w-7xl mx-auto px-6 py-10 grid gap-6 md:grid-cols-3 md:items-center">
         <div className="flex items-center gap-3">
-          <div className="relative h-20 w-30 rounded-2xl overflow-hidden ">
+          <div className="relative h-20 w-60 rounded-2xl overflow-hidden ">
             <Image
               src="/logo.png"
               alt="AR Productions Logo"
@@ -14,9 +14,7 @@ export default function Footer() {
             />
           </div>
           <div>
-            <p className="text-sm font-semibold text-[#2857ff]">
-              AR Productions
-            </p>
+            <p className="text-sm font-semibold">AR Productions</p>
             <p className="mt-3 text-sm text-slate-600">
               Premium software, product design, and enterprise development
               services.
